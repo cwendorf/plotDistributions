06b41a23dda9b2726d0309e08809cc3c7672a6aa
cwendorf <45096238+cwendorf@users.noreply.github.com>
2022-03-03 06:32:14 -0600

    Update plotDistributionsExamples.R

--- docs/plotDistributionsExamples.R
+++ docs/plotDistributionsExamples.R
@@ -9,7 +9,9 @@ norm.pdf(probs=c(.025,.975))
 norm.pdf(probs=c(.5,.9999))
 
 norm.cdf()
+norm.cdf(limits=c(2))
 norm.cdf(limits=c(1,2))
+norm.cdf(probs=c(.95))
 norm.cdf(probs=c(.025,.975))
 
 ### Student's Distribution
@@ -19,17 +21,21 @@ t.pdf(params=c(df=5),limits=c(1,2))
 t.pdf(params=c(df=5),probs=c(.025,.975))
 
 t.cdf(params=c(df=5))
+t.cdf(params=c(df=5),limits=c(2))
 t.cdf(params=c(df=5),limits=c(1,2))
+t.cdf(params=c(df=5),probs=c(.95))
 t.cdf(params=c(df=5),probs=c(.025,.975))
 
 ### F Distribution
 
 f.pdf(params=c(df1=2,df2=10))
-f.pdf(params=c(df1=2,df2=10),limits=c(1,1000))
+f.pdf(params=c(df1=2,df2=10),limits=c(3,50))
 f.pdf(params=c(df1=2,df2=10),probs=c(.95,.99999))
 
 f.cdf(params=c(df1=2,df2=10))
-f.cdf(params=c(df1=2,df2=10),limits=c(1,1000))
+f.cdf(params=c(df1=2,df2=10),limits=c(3))
+f.cdf(params=c(df1=2,df2=10),limits=c(3,50))
+f.cdf(params=c(df1=2,df2=10),probs=c(.95))
 f.cdf(params=c(df1=2,df2=10),probs=c(.95,.99999))
 
 ### Studentized Range Distribution
@@ -39,16 +45,18 @@ q.pdf(params=c(nmeans=2,df=10),limits=c(1,1000))
 q.pdf(params=c(nmeans=2,df=10),probs=c(.95,.99999))
 
 q.cdf(params=c(nmeans=2,df=10))
+q.cdf(params=c(nmeans=2,df=10),limits=c(1))
 q.cdf(params=c(nmeans=2,df=10),limits=c(1,1000))
 q.cdf(params=c(nmeans=2,df=10),probs=c(.95,.99999))
 
 ### Chi Square Distribution
 
 chisq.pdf(params=c(df=10))
-chisq.pdf(params=c(df=10),limits=c(10,1000))
+chisq.pdf(params=c(df=10),limits=c(10,40))
 chisq.pdf(params=c(df=10),probs=c(.95,.99999))
 
 chisq.cdf(params=c(df=10))
-chisq.cdf(params=c(df=10),limits=c(10,1000))
+chisq.cdf(params=c(df=10),limits=c(10))
+chisq.cdf(params=c(df=10),limits=c(10,40))
+chisq.cdf(params=c(df=10),probs=c(.95))
 chisq.cdf(params=c(df=10),probs=c(.95,.99999))
-
