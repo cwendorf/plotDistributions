af8f6433015459be367fdb9a2f0c47f5d3475857
Craig Wendorf <cwendorf@uwsp.edu>
2025-06-25 14:01:32 -0500

    Updated documentation

---
+++ R/plotDistributions-package.R
@@ -0,0 +1,17 @@
+#' plotDistributions: Visualizing Probability Distributions
+#'
+#' plotDistributions provides functions to plot and visualize continuous distributions.
+#'
+#' @details
+#' plotDistributions is a small R package for plotting the probability density and 
+#' cumulative distribution functions for various continuous distributions. 
+#' It provides functionality to highlight probabilities and display critical values.
+#'
+#' @author
+#' **Maintainer**: Craig Wendorf 
+#' \email{cwendorf@uwsp.edu}
+#'
+#' @docType package
+#' @name plotDistributions
+#' @aliases plotDistributions-package
+"_PACKAGE"
