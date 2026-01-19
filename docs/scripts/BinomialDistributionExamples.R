0e0bf0ef125de4045a8ab900ea52e3f19521b01e
Craig Wendorf <cwendorf@uwsp.edu>
2026-01-19 08:12:47 -0600

    Update BinomialDistributionExamples.R

--- docs/scripts/BinomialDistributionExamples.R
+++ docs/scripts/BinomialDistributionExamples.R
@@ -5,12 +5,12 @@ source("http://raw.githubusercontent.com/cwendorf/plotDistributions/main/source-
 
 ### Probability Density Function
 
-binom.pdf(params = c(size = 10, prob = .4))
-binom.pdf(params = c(size = 10, prob = .4), limits = c(4, 7))
-binom.pdf(params = c(size = 10, prob = .4), probs = c(.2, .8))
+binom.pdf(params = c(size = 10, prob = .5))
+binom.pdf(params = c(size = 10, prob = .5), limits = c(4, 7))
+binom.pdf(params = c(size = 10, prob = .5), probs = c(.2, .8))
 
 ### Cumulative Distribution Function
 
-binom.cdf(params = c(size = 10, prob = .4))
-binom.cdf(params = c(size = 10, prob = .4), limits = c(4, 7))
-binom.cdf(params = c(size = 10, prob = .4), probs = c(.2, .8))
+binom.cdf(params = c(size = 10, prob = .5))
+binom.cdf(params = c(size = 10, prob = .5), limits = c(4, 7))
+binom.cdf(params = c(size = 10, prob = .5), probs = c(.2, .8))
